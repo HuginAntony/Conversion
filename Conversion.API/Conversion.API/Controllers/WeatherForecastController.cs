06684122a32cb16204ca1e65377e19b154c18a5e
HuginPonnusamy <hugin.ponnusamy@absolutesys.com>
2021-02-02 17:20:54 +0200

    Adding Conversion.API web project

---
+++ Conversion.API/Conversion.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Conversion.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConversionController : ControllerBase
+    {
+        private readonly ILogger<ConversionController> _logger;
+
+        public ConversionController(ILogger<ConversionController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
