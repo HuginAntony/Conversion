00cbf14a371d5f10cfbbf70bed9f361721070457
HuginPonnusamy <hugin.ponnusamy@absolutesys.com>
2021-02-03 17:27:30 +0200

    Recreating Angular app in version 10, because 11 has breaking changes

--- conversion-website/src/app/app.component.spec.ts
+++ conversion-website/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'conversion-web'`, () => {
+  it(`should have as title 'conversion-website'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('conversion-web');
+    expect(app.title).toEqual('conversion-website');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('conversion-web app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('conversion-website app is running!');
   });
 });
