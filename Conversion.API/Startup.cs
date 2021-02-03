81e034c59887bacdd97ecb7399f06d666b6dc093
HuginPonnusamy <hugin.ponnusamy@absolutesys.com>
2021-02-03 10:46:11 +0200

    Adding configuration keys for Database connection string and JWT Adding Identity service Configuring Swagger UI Adding Authentication, Swagger and Cors  middleware Setting up dependency injection for ConversionDbContext

--- Conversion.API/Startup.cs
+++ Conversion.API/Startup.cs
@@ -13,6 +13,14 @@ using System.Threading.Tasks;
 using Conversion.Core;
 using Conversion.Core.Contracts;
 using Conversion.Core.Converters;
+using Conversion.DataAccess;
+using Conversion.DataAccess.Core;
+using Conversion.DataAccess.Interfaces;
+using Conversion.DataAccess.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Conversion.API
 {
@@ -33,6 +41,32 @@ namespace Conversion.API
             services.AddScoped<ITemperatureConverter, TemperatureConverter>();
             services.AddScoped<IMassConverter, MassConverter>();
             services.AddScoped<ILengthConverter, LengthConverter>();
+
+            ConfigureDatabase(services);
+
+            services.AddScoped<IUnitOfWork<ConversionDbContext>, UnitOfWork<ConversionDbContext>>();
+
+            // Add Identity service
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                    .AddEntityFrameworkStores<ConversionDbContext>()
+                    .AddDefaultTokenProviders();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Conversion REST API",
+                    Description = "Used to get conversions from metric to imperial and vice versa"
+                });
+
+                c.AddFluentValidationRules();
+            });
+        }
+
+        public virtual void ConfigureDatabase(IServiceCollection services)
+        {
+            services.AddDbContext<ConversionDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ConversionDB")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,8 +79,20 @@ namespace Conversion.API
 
             app.UseHttpsRedirection();
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Conversion Web API v1");
+            });
+
+            app.UseCors(x => x.WithOrigins("http://localhost:4200")
+                              .AllowAnyMethod()
+                              .AllowAnyHeader());
+
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
