7a5b2bce468fbec7752e58aaefb63509853f749e
HuginPonnusamy <hugin.ponnusamy@absolutesys.com>
2021-02-03 10:32:42 +0200

    Adding Conversion.DataAccess project Adding a generic Repository and UnitOfWork implementation Adding ApplicationUser for user Identity Adding ConversionDbContext class

---
+++ Conversion.DataAccess/ConversionDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Conversion.DataAccess.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Conversion.DataAccess
+{
+    public class ConversionDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ConversionDbContext()
+        {
+            
+        }
+
+        public ConversionDbContext(DbContextOptions<ConversionDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
